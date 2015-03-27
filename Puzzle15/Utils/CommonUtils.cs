4c3c1c8a8b1cba64aab5d5234b48ca2831c452f6
Dmitry G. Barabash <dmitry@barabash.com>
2015-03-27 13:08:09 +0200

    Small refactoring of CommonUtils class
    --HG--
    branch : develop

---
+++ Puzzle15/Utils/CommonUtils.cs
@@ -0,0 +1,15 @@
+﻿namespace Puzzle15.Utils
+{
+    public static class CommonUtils
+    {
+        public static string GetMovesWord(uint moves)
+        {
+            uint afterLast = moves % 100 / 10;
+            uint last = moves % 10;
+
+            if (afterLast == 1 || last == 0 || (last >= 5 && last <= 9)) return "ходов";
+            if (last == 1) return "ход";
+            if (last >= 2 && last <= 4) return "хода";
+        }
+    }
+}
