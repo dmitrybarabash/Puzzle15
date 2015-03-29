2b451582536150af2fac70c13aec5d02471385c4
Dmitry G. Barabash <dmitry@barabash.com>
2015-03-29 14:02:01 +0300

    Added Score.Equals methods and tests for BestScoresStorage class
    --HG--
    branch : develop

--- Puzzle15.Tests/BestScoresTests.cs
+++ Puzzle15.Tests/BestScoresTests.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.IO;
 using NUnit.Framework;
-using Puzzle15.Common;
 using Puzzle15.DomainModel;
 
 namespace Puzzle15.Tests
@@ -78,28 +76,6 @@ namespace Puzzle15.Tests
             bestScores.Add(score);
 
             return bestScores.Scores[expectedIndex] == score;
-        }
-
-        [Test]
-        public void Save_Nothing_Saves()
-        {
-            var bestScores = new BestScores();
-            bestScores.Add(new Score() { Name = "Aaaaaaaaaa", Moves = 92, Timer = new TimeSpan(0, 0, 11, 25) });
-            bestScores.Add(new Score() { Name = "Bbbbbbbbb", Moves = 102, Timer = new TimeSpan(0, 0, 12, 25) });
-            bestScores.Add(new Score() { Name = "Cccccc", Moves = 103, Timer = new TimeSpan(0, 0, 13, 25) });
-            bestScores.Add(new Score() { Name = "Dmitrik", Moves = 124, Timer = new TimeSpan(0, 0, 14, 25) });
-            bestScores.Add(new Score() { Name = "Eeeeee", Moves = 135, Timer = new TimeSpan(0, 0, 15, 25) });
-            bestScores.Add(new Score() { Name = "Ffffffffffff", Moves = 146, Timer = new TimeSpan(0, 0, 16, 25) });
-            bestScores.Add(new Score() { Name = "Gggg", Moves = 147, Timer = new TimeSpan(0, 1, 17, 25) });
-            bestScores.Add(new Score() { Name = "Hhhhhhh", Moves = 158, Timer = new TimeSpan(0, 1, 18, 25) });
-            bestScores.Add(new Score() { Name = "Iiiiiiii", Moves = 159, Timer = new TimeSpan(0, 1, 19, 25) });
-            bestScores.Add(new Score() { Name = "Jjjjjjj", Moves = 200, Timer = new TimeSpan(0, 1, 20, 25) });
-
-            new BestScoresStorage(Utils.BestScoresStorageFileName).Save(bestScores);
-
-            bool actual = File.Exists(Utils.BestScoresStorageFileName);
-
-            Assert.That(actual, Is.True);
         }
     }
 }
