5cbb5f8e78dc5bd5655812e5d0a7d2c489485d8f
Dmitry G. Barabash <dmitry@barabash.com>
2015-04-01 12:05:51 +0300

    Added some tests for presenters
    --HG--
    branch : develop

---
+++ Puzzle15.Tests/PuzzlePresenterTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Puzzle15.Tests
+{
+    [TestFixtureAttribute]
+    class PuzzlePresenterTests
+    {
+        [Test]
+        public void NewGame()
+        {
+
+        }
+
+        [Test]
+        public void Move()
+        {
+
+        }
+    }
+}
