9f30bf93873a39e1e15f3ccf9c7b66f0375fec28
Dmitry G. Barabash <dmitry@barabash.com>
2015-07-07 07:06:37 +0300

    Removed XML documentation comments to Puzzle15.Common namespace
    --HG--
    branch : develop

--- Puzzle15/Common/IPresenter.cs
+++ Puzzle15/Common/IPresenter.cs
@@ -1,14 +1,7 @@
 ﻿namespace Puzzle15.Common
 {
-    /// <summary>
-    /// Общий интерфейс всех презентеров.
-    /// </summary>
-    /// <typeparam name="T">Тип представления.</typeparam>
     public interface IPresenter<T> where T : IView
     {
-        /// <summary>
-        /// Возвращает ссылку на представление.
-        /// </summary>
         T View { get; set; }
     }
 }
