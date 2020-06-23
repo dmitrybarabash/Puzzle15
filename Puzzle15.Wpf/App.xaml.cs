7c1a43874f681c45e836c3bb336867160d74a528
Dmitry G. Barabash <dmitry@barabash.com>
2020-06-23 18:08:17 +0300

    Added WPF windows

--- Puzzle15.Wpf/App.xaml.cs
+++ Puzzle15.Wpf/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
 
 namespace Puzzle15.Wpf
@@ -13,5 +8,39 @@ namespace Puzzle15.Wpf
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Создаем окно splash screen, временно делаем его главным
+            // окном приложения и показываем его пользователю
+            var splashScreen = new SplashScreenWindow();
+            MainWindow = splashScreen;
+            splashScreen.Show();
+
+            // Чтобы UI оставался отзывчивым, асинхронно делаем паузу
+            // и потом переходим к показу настоящего главного окна
+            Task.Factory.StartNew(() =>
+            {
+                // Делаем паузу
+                System.Threading.Thread.Sleep(3000);
+
+                // Создаем настоящее главное окно типа MainWindow, делаем его главным окном приложения
+                // и показываем его пользователю. Поскольку мы сейчас вне основного UI-потока, то обращение
+                // к тому, что имеет отношение UI-потоку должно быть вызвано потокобезопасно. В WPF это
+                // делается при помощи метода Dispatcher.Invoke.
+                Dispatcher.Invoke(() =>
+                {
+                    // Создаем настоящее главное окно типа MainWindow, делаем его
+                    // главным окном приложения и показываем его пользователю
+                    var mainWindow = new MainWindow();
+                    MainWindow = mainWindow;
+                    mainWindow.Show();
+
+                    // Закрываем splash screen
+                    splashScreen.Close();
+                });
+            });
+        }
     }
 }
---
+++ Puzzle15.Wpf/SplashScreenWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Puzzle15.Wpf
+{
+    /// <summary>
+    /// Interaction logic for SplashScreenWindow.xaml
+    /// </summary>
+    public partial class SplashScreenWindow : Window
+    {
+        public SplashScreenWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
