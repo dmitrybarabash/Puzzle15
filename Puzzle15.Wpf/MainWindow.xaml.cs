fec539869870528617575a7a67c739a8550321d2
Dmitry G. Barabash <dmitry@barabash.com>
2020-06-24 00:20:36 +0300

    Added main and context menus in WPF main windows

--- Puzzle15.Wpf/MainWindow.xaml.cs
+++ Puzzle15.Wpf/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Puzzle15.Wpf
 {
@@ -30,19 +17,24 @@ namespace Puzzle15.Wpf
             //
         }
 
-        private void ButtonNewGame_Click(object sender, RoutedEventArgs e)
+        private void BestScores_Click(object sender, RoutedEventArgs e)
         {
             //
         }
 
-        private void ButtonAbout_Click(object sender, RoutedEventArgs e)
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
         {
             // Свойство Owner для модального диалога необходимо установить для того, чтобы диалог
             // действительно стал модальным относительно родительского окна и не был виден по Alt+Tab
             new AboutWindow { Owner = this }.ShowDialog();
         }
 
-        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
