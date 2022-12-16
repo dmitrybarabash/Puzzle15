557b96fb659d4c383cc9ab19362604cd26aa1515
Dmitry Barabash <dmitry@barabash.com>
2022-12-16 20:32:32 +0000

    Test: Style refactoring

--- tests/Puzzle15.WinForms.Mvp.UnitTests/Stubs/StubBestScoresStorage.cs
+++ tests/Puzzle15.WinForms.Mvp.UnitTests/Stubs/StubBestScoresStorage.cs
@@ -11,19 +11,19 @@ public class StubBestScoresStorage : IBestScoresStorage
     public StubBestScoresStorage(string fileName)
     {
         FileName = fileName;
-        scores = new List<Score>()
-            {
-                new Score { Name = "Aaaaaaaaaa", Moves = 92, Timer = new TimeSpan(0, 0, 11, 25) },
-                new Score { Name = "Bbbbbbbbb", Moves = 102, Timer = new TimeSpan(0, 0, 12, 25) },
-                new Score { Name = "Cccccc", Moves = 103, Timer = new TimeSpan(0, 0, 13, 25) },
-                new Score { Name = "Dmitrik", Moves = 124, Timer = new TimeSpan(0, 0, 14, 25) },
-                new Score { Name = "Eeeeee", Moves = 135, Timer = new TimeSpan(0, 0, 15, 25) },
-                new Score { Name = "Ffffffffffff", Moves = 146, Timer = new TimeSpan(0, 0, 16, 25) },
-                new Score { Name = "Gggg", Moves = 147, Timer = new TimeSpan(0, 1, 17, 25) },
-                new Score { Name = "Hhhhhhh", Moves = 158, Timer = new TimeSpan(0, 1, 18, 25) },
-                new Score { Name = "Iiiiiiii", Moves = 159, Timer = new TimeSpan(0, 1, 19, 25) },
-                new Score { Name = "Jjjjjjj", Moves = 200, Timer = new TimeSpan(0, 1, 20, 25) }
-            };
+        scores = new List<Score>
+        {
+            new Score { Name = "Aaaaaaaaaa", Moves = 92, Timer = new TimeSpan(0, 0, 11, 25) },
+            new Score { Name = "Bbbbbbbbb", Moves = 102, Timer = new TimeSpan(0, 0, 12, 25) },
+            new Score { Name = "Cccccc", Moves = 103, Timer = new TimeSpan(0, 0, 13, 25) },
+            new Score { Name = "Dmitrik", Moves = 124, Timer = new TimeSpan(0, 0, 14, 25) },
+            new Score { Name = "Eeeeee", Moves = 135, Timer = new TimeSpan(0, 0, 15, 25) },
+            new Score { Name = "Ffffffffffff", Moves = 146, Timer = new TimeSpan(0, 0, 16, 25) },
+            new Score { Name = "Gggg", Moves = 147, Timer = new TimeSpan(0, 1, 17, 25) },
+            new Score { Name = "Hhhhhhh", Moves = 158, Timer = new TimeSpan(0, 1, 18, 25) },
+            new Score { Name = "Iiiiiiii", Moves = 159, Timer = new TimeSpan(0, 1, 19, 25) },
+            new Score { Name = "Jjjjjjj", Moves = 200, Timer = new TimeSpan(0, 1, 20, 25) }
+        };
     }
 
     #region IBestScoresStorage implementation
